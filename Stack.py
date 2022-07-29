c49ca48be1a1b84cf940ff041b7849bcfb8658d3
Mayakshanesht <mayakshanesht1995@gmail.com>
2022-07-29 16:26:19 +0530

    Updated stack with push and pop commands

--- Stack.py
+++ Stack.py
@@ -10,29 +10,35 @@ class stack(object):
     def __init__(self):
         self.obj=LList()
     def push(self,node):
-        if self.obj.head==None:
-            self.obj.head=node
+        temp=self.obj.head
+        if temp!=None:       
+            while temp.next!=None:      
+                temp=temp.next
+            temp.next=node         
         else:
-            temp2=self.obj.head
-            temp2.next=node
             self.obj.head=node
-
     def printl(self):
         A=[]
         temp=self.obj.head
         if temp:
             while temp:
                 A.append(temp.value)
+                print(temp.value)
                 temp=temp.next
         print(A)
-            
-       
-    #def pop(self):
-    #    temp=self.obj.head
-    #    if temp!=None:
-    #        while temp.next:
-    #            temp=temp.next
-    #        deleted_element=temp.next
+    def pop(self):
+        temp=self.obj.head
+        if temp!=None:
+            if temp.next!=None:
+                while temp.next!=None:
+                    temp2=temp
+                    temp=temp.next 
+                temp2.next=None 
+            else:
+                self.obj.head=None
+                print('hi')
+        else:
+            print('No element in this Linkedlist to pop')
 if __name__=='__main__':
     node=Node(1)
     obj2=stack()
@@ -41,8 +47,32 @@ if __name__=='__main__':
     obj2.push(node2)
     node3=Node(3)
     obj2.push(node3)
-    obj2.printl()
     
+    node4=Node(4)
+    obj2.push(node4)
+    node5=Node(5)
+    obj2.push(node5)
+    node6=Node(6)
+    obj2.push(node6)
+    obj2.printl()
+    obj2.pop()
+    obj2.printl()
+    obj2.pop()
+    obj2.printl()
+    obj2.pop()
+    obj2.printl()
+    obj2.pop()
+    obj2.printl()
+    obj2.pop()
+    obj2.printl()
+    obj2.pop()
+    obj2.printl()
+    obj2.pop()
+    obj2.printl()
+    node7=Node(7)
+    obj2.push(node7)
+    obj2.printl()
+
 
 
 
